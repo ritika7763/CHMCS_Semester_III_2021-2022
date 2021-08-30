4e7cd88358cb1afdc9d316b2ab985da1af8f4c00
ritika7763 <87898771+ritika7763@users.noreply.github.com>
2021-08-30 18:14:53 +0530

    Add files via upload

---
+++ Prac_08_RS_ 30_08_2021/P8_PR_FIFO_RS.java
@@ -0,0 +1,75 @@
+QUESTION:
+Write a java program that implements  the FIFO page-replacement algorithm.
+
+IMPLEMENTATION:
+
+//Name:Ritika Sahu
+//Batch : B1
+//PRN:2020016400783543
+//Date: 30 August, 2021.
+//Practical 8: Page Repalcenment Algorithm FIFO
+
+import java.io.*;
+import java.util.*;
+public class P8_PR_FIFO_RS
+{
+   public static void main(String[] args) throws IOException
+   {
+     Scanner scan = new Scanner(System.in);
+     int frames, pointer = 0, hit = 0, fault = 0, ref_len;
+     int buffer[];
+     int reference[];
+     int mem_layout[][];
+     System.out.print("Please enter the number of frames: ");
+     frames = scan.nextInt();
+
+     System.out.print("Please enter the length of reference string: ");
+     ref_len = scan.nextInt();
+
+     reference = new int[ref_len];
+     mem_layout = new int[ref_len][frames];
+     buffer = new int[frames];
+     for(int j = 0;j < frames;j++)
+            buffer[j] = -1;
+
+    System.out.print("Please enter the reference string: ");
+    for(int i = 0; i < ref_len;i++)
+    {
+           reference[i] = scan.nextInt();
+    }
+     System.out.printIn();
+     for(int i = 0;i < ref_len;i++)
+     {
+        int search = -1;
+        for(int j = 0;j < frames;j++)
+        {
+              if (buffer[j] == reference[i])
+              {
+                   search = j;
+                   hit++;
+                   break;
+             }
+        }
+       if (search == -1)
+          {
+                buffer[pointer] = reference[i];
+                fault++;
+                pointer++;
+                if(pointer == frames)
+                     pointer = 0;
+          }
+          for(int j = 0;j < frames;j++)
+                mem_layout[i][j] = buffer[j];
+         }
+         for(int i = 0;i < frames;i++)
+         {
+               for(int j = 0;j < ref_len;j++)
+                       System.out.printf("%3d",mem_layout[j][i]);
+               System.out.printIn();
+         }
+
+           System.out.printIn("The number of Hits: " + hit);
+           System.out.printIn("Hit Ratio: " + (float)((float)hit/ref_len));
+           System.out.printIn("The number of Faults: " + fault);
+   }
+}
